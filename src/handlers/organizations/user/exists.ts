c42219b2a4f3ffddd4a94b2f45dc3f2236a3144f
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2023-01-07 22:15:49 +0300

    Update exists.ts

--- src/handlers/organizations/user/exists.ts
+++ src/handlers/organizations/user/exists.ts
@@ -3,17 +3,32 @@ import "source-map-support/register";
 import middy from "@middy/core";
 import { diContainer } from "../../../core/di-registry";
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { OrganizationService } from "../../../services/organization.service";
 
 const lambdaHandler = async function (
-  event: APIGatewayEvent,
+  event: any,
   context: Context
 ): Promise<APIGatewayProxyResult> {
   // Print event
   console.log(JSON.stringify(event));
+  const organizationService = diContainer.resolve(OrganizationService);
+  const organizationId = event["OrganizationId"] as string;
+  const userId = event["UserId"] as string;
+
+  console.log("Calling data is : ", {
+    OrganizationId: organizationId,
+    UserId: userId,
+  });
+
+  const doesOrganizationHasUser = await organizationService.hasUser(
+    organizationId,
+    userId
+  );
+  console.log("Result is :", doesOrganizationHasUser);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: `${doesOrganizationHasUser}`,
     headers: {
       "Content-Type": "application/json",
     },
